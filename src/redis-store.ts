7f849b8ff3bb2760d0a2853ee0e8b3e49e91915d
Michael <Michael@MichaelHumiston.com>
2022-09-03 19:07:20 -0400

    Fix tests

--- src/redis-store.ts
+++ src/redis-store.ts
@@ -55,7 +55,7 @@ export default class RedisStore implements SessionStore {
     const getSession = promisify(this.client.get).bind(this.client);
 
     const session: any = await getSession(`${this.opts.prefix}-${id}`);
-    const values: SessionValues = <SessionValues> JSON.parse(session);
+    const values: SessionValues = JSON.parse(session) as SessionValues;
 
     return values;
 
